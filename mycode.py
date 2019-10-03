b4f97c03bcb149af1ac2473374d599bfa8818264
Duska Barisic <duskabarisic@yahoo.com>
2019-10-03 14:41:25 +0200

    My Django Girls app, first commit

---
+++ mycode.py
@@ -0,0 +1,18 @@
+def hi():
+    print('Hi there!')
+    print('How are you?')
+
+hi()
+def hi(name):
+    if name == 'Duska':
+        print('Cao Duska')
+    elif name == 'Jelena':
+        print('Cao Jelena')
+    else:
+        print('cao Neko')
+
+hi('Jelena')
+
+        
+
+    
